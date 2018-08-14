ce2ea99bf6464e41b123b18dfb5d60a1c18c0ca3
Lijue <boting87@gmail.com>
2018-08-14 12:12:43 +0800

    initial

---
+++ app/src/main/java/com/hanyutech/hanyuiot01/Datadisplay.java
@@ -0,0 +1,17 @@
+package com.hanyutech.hanyuiot01;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class Datadisplay extends AppCompatActivity {
+
+    private FirebaseDatabase database = FirebaseDatabase.getInstance();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_datadisplay);
+    }
+}
---
+++ app/src/main/java/com/hanyutech/hanyuiot01/MainActivity.java
@@ -0,0 +1,34 @@
+package com.hanyutech.hanyuiot01;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Display;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private Button Datadisplay,Wifi_connetc;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Datadisplay = findViewById(R.id.DataDisplay);
+        Datadisplay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, com.hanyutech.hanyuiot01.Datadisplay.class);
+                startActivity(intent);
+            }
+        });
+        Wifi_connetc = findViewById(R.id.Wificonnect);
+        Wifi_connetc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,EsptouchDemoActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
