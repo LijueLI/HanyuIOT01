f23d2f307c9e4e699bc92ede7859156667ef14b8
LijueLI <32730248+LijueLI@users.noreply.github.com>
2018-08-24 01:22:43 +0800

    ver2改版

--- app/src/main/java/com/hanyutech/hanyuiot01/Datadisplay.java
+++ app/src/main/java/com/hanyutech/hanyuiot01/Datadisplay.java
@@ -11,6 +11,12 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -23,58 +29,55 @@ import org.w3c.dom.Comment;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Datadisplay extends AppCompatActivity {
+public class Datadisplay extends AppCompatActivity implements OnMapReadyCallback{
 
     private FirebaseDatabase database = FirebaseDatabase.getInstance();
-    private TextView temp,humi,lan,lon;
-    private Button map;
-    private List<Data> Datals = new ArrayList<Data>();
-    private double latitude = 0,longitude = 0;
+    private TextView temp,humi,lat,lon,pm25,pm10,pm100;
+    private MapView mapView;
+    private GoogleMap googleMap;
+    private static final String MAP_VIEW_BUNDLE_KEY = "AIzaSyCnK90HIqo4kvvH6-wFqvJbWGHmWfdBqvY";
+    private Double Longitude = 0.0,Latitude = 0.0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_datadisplay);
 
-        temp = findViewById(R.id.temperature);
-        humi = findViewById(R.id.humidity);
-        lan = findViewById(R.id.lan);
-        lon = findViewById(R.id.lon);
-        map = findViewById(R.id.map);
+        Bundle mapViewBundle = null;
+        if(savedInstanceState != null){
+            mapViewBundle = savedInstanceState.getBundle(MAP_VIEW_BUNDLE_KEY);
+        }
+        mapView = findViewById(R.id.mapView);
+        mapView.onCreate(mapViewBundle);
+        mapView.getMapAsync(this);
 
-        DatabaseReference Obj = database.getReference("object");
+        temp = findViewById(R.id.Temp_Value);
+        humi = findViewById(R.id.Humi_Value);
+        lat = findViewById(R.id.Lat_Value);
+        lon = findViewById(R.id.Long_Value);
+        pm10 = findViewById(R.id.PM10_Value);
+        pm25 = findViewById(R.id.PM25_Value);
+        pm100 = findViewById(R.id.PM100_Value);
 
-        Obj.addChildEventListener(new ChildEventListener() {
+
+        DatabaseReference Obj = database.getReference("test1");
+        DatabaseReference PM10 = Obj.child("PM10");
+        DatabaseReference PM100 = Obj.child("PM100");
+        DatabaseReference PM25 = Obj.child("PM25");
+        DatabaseReference flat = Obj.child("flat");
+        DatabaseReference flon = Obj.child("flon");
+        final DatabaseReference humidity = Obj.child("humidity");
+        DatabaseReference temperature = Obj.child("temperature");
+
+        Obj.addValueEventListener(new ValueEventListener() {
             @Override
-            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
-                Log.d("fire", "onChildAdded:" + dataSnapshot.getKey());
-                Log.d("fire",dataSnapshot.child("flat").getValue(String.class));
-                Data d = new Data(0,0,0,0);
-                try {
-                    d.setHumidity(Double.parseDouble(dataSnapshot.child("humidity").getValue(String.class)));
-                    d.setTempture(Double.parseDouble(dataSnapshot.child("temperature").getValue(String.class)));
-                    d.setLatitude(Double.parseDouble(dataSnapshot.child("flat").getValue(String.class)));
-                    d.setLongitude(Double.parseDouble(dataSnapshot.child("flon").getValue(String.class)));
-                }catch (NumberFormatException e){}
-                Datals.add(d);
-                temp.setText(String.valueOf(Datals.get(Datals.size()-1).getTempture()));
-                humi.setText(String.valueOf(Datals.get(Datals.size()-1).getHumidity()));
-                lan.setText(String.valueOf(Datals.get(Datals.size()-1).getLatitude()));
-                lon.setText(String.valueOf(Datals.get(Datals.size()-1).getLongitude()));
-            }
-
-            @Override
-            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
-
-            }
-
-            @Override
-            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
-
-            }
-
-            @Override
-            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
-
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                Latitude=Double.parseDouble(dataSnapshot.child("flat").getValue(String.class));
+                Longitude=Double.parseDouble(dataSnapshot.child("flon").getValue(String.class));
+                googleMap.clear();
+                LatLng current = new LatLng(Latitude,Longitude);
+                googleMap.addMarker(new MarkerOptions().position(current).title(Latitude.toString()+"  "+Longitude.toString()));
+                googleMap.moveCamera(CameraUpdateFactory.newLatLng(current));
             }
 
             @Override
@@ -83,13 +86,143 @@ public class Datadisplay extends AppCompatActivity {
             }
         });
 
-        map.setOnClickListener(new View.OnClickListener() {
+        PM10.addValueEventListener(new ValueEventListener() {
             @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(Datadisplay.this,Map.class);
-                startActivity(intent);
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                pm10.setText(dataSnapshot.getValue().toString());
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+        PM100.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                pm100.setText(dataSnapshot.getValue().toString());
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+        PM25.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                pm25.setText(dataSnapshot.getValue().toString());
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+        flat.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                lat.setText(dataSnapshot.getValue().toString());
+                Latitude = Double.parseDouble(dataSnapshot.getValue(String.class));
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+        flon.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                lon.setText(dataSnapshot.getValue().toString());
+                Longitude = Double.parseDouble(dataSnapshot.getValue(String.class));
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+        humidity.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                humi.setText(dataSnapshot.getValue().toString());
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+        temperature.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                temp.setText(dataSnapshot.getValue().toString());
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
             }
         });
     }
 
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        Bundle mapViewBundle = outState.getBundle(MAP_VIEW_BUNDLE_KEY);
+        if(mapViewBundle == null){
+            mapViewBundle = new Bundle();
+            outState.putBundle(MAP_VIEW_BUNDLE_KEY,mapViewBundle);
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mapView.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mapView.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        mapView.onDestroy();
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onPause() {
+        mapView.onPause();
+        super.onPause();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        this.googleMap = googleMap;
+        this.googleMap.setMinZoomPreference(10);
+    }
 }
